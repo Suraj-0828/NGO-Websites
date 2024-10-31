4fc189edbaf920e9145d0112d5235ec5980bbe69
Suraj Jha <134250774+Suraj-0828@users.noreply.github.com>
2024-10-31 12:46:36 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,251 @@
+<!-- Start Footer Area -->
+<footer class="footer-section pt-100">
+        <div class="container">
+            <div class="subscribe-area">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-md-6">
+                        <div class="subscribe-content">
+                            <h2>Join Our Newsletter</h2>
+                            <p>News letter dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6">
+                        <form class="newsletter-form">
+                            <input type="email" class="input-newsletter" placeholder="Enter your email" name="EMAIL" required autocomplete="off">
+                            <button type="submit">
+                                Subscribe Now
+                                <i class="flaticon-right"></i>
+                            </button>
+                            <div id="validator-newsletter" class="form-result"></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h3>Contact info</h3>
+
+                        <div class="footer-info-contact">
+                            <i class="flaticon-pin"></i>
+                            <h3>Location</h3>
+                            <span>205 Fida Walinton, Tongo<br> New York, Canada</span>
+                        </div>
+
+                        <div class="footer-info-contact">
+                            <i class="flaticon-call"></i>
+                            <h3>Call Us</h3>
+                            <span><a href="tel:+882-569-756">987-0986-0987</a></span>
+                        </div>
+
+                        <div class="footer-info-contact">
+                            <i class="flaticon-email"></i>
+                            <h3>Email Us</h3>
+                            <span>
+                                <a href="mailto:hello@huruma.com">
+                                    support@huruma.com
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h3>Our Support</h3>
+
+                        <ul class="footer-quick-links">
+                            <li>
+                                <a href="causes.html">
+                                    Private Policies
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Donate Now
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Become a Volunteer
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Careers
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Contact Us
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Paid programs
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Partnership
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h3>Our Services</h3>
+
+                        <ul class="footer-quick-links">
+                            <li>
+                                <a href="causes.html">
+                                    Donate
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Sponsor
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Fundraise
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Volunteer
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Partner
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Jobs
+                                </a>
+                            </li>
+                            <li>
+                                <a href="causes.html">
+                                    Form
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-footer-widget">
+                        <h3>Recent News</h3>
+
+                        <div class="footer-news">
+                            <a href="single-blog.html">
+                                <img src="assets/img/footer-news/1.jpg" alt="image">
+                                <h4>Support about poverty to poor family</h4>
+                                <span>Poor, 22 January</span>
+                            </a>
+                        </div>
+
+                        <div class="footer-news">
+                            <a href="single-blog.html">
+                                <img src="assets/img/footer-news/2.jpg" alt="image">
+                                <h4>Mostly suffered school Boys care</h4>
+                                <span>Health, 24 February</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="copyright-area">
+            <div class="container">
+                <div class="copyright-area-content">
+                    <div class="row align-items-center">
+                        <div class="col-lg-4">
+                            <div class="copyright-logo">
+                                <img src="assets/img/white-logo.png" alt="image">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6">
+                            <p>© Huruma is Proudly Owned by <a href="https://envytheme.com/" target="_blank">EnvyTheme</a></p>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6">
+                            <ul>
+                                <li>
+                                    <a href="terms-condition.html">Terms & Conditions</a>
+                                </li>
+                                <li>
+                                    <a href="privacy-policy.html">Privacy Policy</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer Area -->
+
+    <!-- Start Go Top Section -->
+    <div class="go-top">
+        <i class="bx bx-chevron-up"></i>
+        <i class="bx bx-chevron-up"></i>
+    </div>
+    <!-- End Go Top Section -->
+
+    <!-- Jquery Slim JS -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <!-- Meanmenu JS -->
+    <script src="assets/js/jquery.meanmenu.js"></script>
+    <!-- Easing Min JS -->
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <!-- Owl Carousel JS -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <!-- Owl Magnific JS -->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <!-- Nice Select JS -->
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <!-- Appear JS -->
+    <script src="assets/js/jquery.appear.js"></script>
+    <!-- Odometer JS -->
+    <script src="assets/js/odometer.min.js"></script>
+    <!-- Image Light Box JS -->
+    <script src="assets/js/imagelightbox.min.js"></script>
+    <!-- Form Ajaxchimp JS -->
+    <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+    <!-- Form Validator JS -->
+    <script src="assets/js/form-validator.min.js"></script>
+    <!-- Contact JS -->
+    <script src="assets/js/contact-form-script.js"></script>
+    <!-- Custom JS -->
+    <script src="assets/js/main.js"></script>
+
+    <!-- Slider Revolution core JavaScript files -->
+    <script src="assets/revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script src="assets/revolution/js/jquery.themepunch.revolution.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script src="assets/revolution/js/extensions/revolution.extension.video.min.js"></script>
+    <script src="assets/js/rev-slider-custom.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,311 @@
+    <!-- Start Navbar Area -->
+    <div class="navbar-area  navbar-style-three">
+        <div class="huruma-responsive-nav">
+            <div class="container">
+                <div class="huruma-responsive-menu">
+                    <div class="logo">
+                        <a href="index.html">
+                            <img src="assets/img/logo-3.png" class="white-logo" alt="logo">
+                            <img src="assets/img/logo-2.png" class="black-logo" alt="image">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="huruma-nav">
+            <div class="container">
+                <nav class="navbar navbar-expand-md navbar-light">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="assets/img/logo-3.png" class="white-logo" alt="logo">
+                        <img src="assets/img/logo-2.png" class="black-logo" alt="image">
+                    </a>
+
+                    <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="index.php" class="nav-link active">
+                                    Home
+                                </a>
+                            </li>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="about-us.php" class="nav-link">
+                                    About Us
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="why-donate.php" class="nav-link">
+                                Why Donate?
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="gallery.php" class="nav-link">
+                                Gallery
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="upcoming-events.php" class="nav-link">
+                                Upcoming Events
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="contact-us.php" class="nav-link">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+
+                        <div class="others-options">                    
+                            <div class="burger-menu">
+                                <i class="flaticon-menu"></i>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- End Navbar Area -->
+
+    <!-- Sidebar Modal -->
+    <div class="sidebar-modal">
+        <div class="sidebar-modal-inner">
+            <div class="sidebar-about-area">
+                <div class="title">
+                    <h2>About Us</h2>
+                    <p>We believe brand interaction is key in communication. Real innovations and a positive customer experience are the heart of successful communication. No fake products and services. The customer is king, their lives and needs are the inspiration.</p>
+                </div>
+            </div>
+
+            <div class="sidebar-instagram-feed">
+                <h2>Instagram</h2>
+                <ul>
+                    <li>
+                        <a href="https://www.instagram.com/" target="_blank">
+                            <img src="assets/img/sidebar/1.jpg" alt="image">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/" target="_blank">
+                            <img src="assets/img/sidebar/2.jpg" alt="image">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/" target="_blank">
+                            <img src="assets/img/sidebar/3.jpg" alt="image">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/" target="_blank">
+                            <img src="assets/img/sidebar/4.jpg" alt="image">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/" target="_blank">
+                            <img src="assets/img/sidebar/5.jpg" alt="image">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/" target="_blank">
+                            <img src="assets/img/sidebar/6.jpg" alt="image">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/" target="_blank">
+                            <img src="assets/img/sidebar/7.jpg" alt="image">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/" target="_blank">
+                            <img src="assets/img/sidebar/8.jpg" alt="image">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="sidebar-contact-area">
+                <div class="contact-info">
+                    <div class="contact-info-content">
+                        <h2>
+                            <a href="tel:+0881306298615">
+                                +088 130 629 8615
+                            </a>
+                            <span>OR</span>
+                            <a href="mailto:huruma@gmail.com">
+                                huruma@gmail.com
+                            </a>
+                        </h2>
+
+                        <ul class="social">
+                            <li>
+                                <a href="https://www.facebook.com/" target="_blank">
+                                    <i class="flaticon-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.twitter.com/" target="_blank">
+                                    <i class="flaticon-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/" target="_blank">
+                                    <i class="flaticon-instagram"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/" target="_blank">
+                                    <i class="flaticon-linkedin"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.pinterest.com/" target="_blank">
+                                    <i class="flaticon-pinterest"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <span class="close-btn sidebar-modal-close-btn">
+                <i class="flaticon-close"></i>
+            </span>
+        </div>
+    </div>
+    <!-- End Sidebar Modal -->
+
+    <!-- Start Home Banner Three Area -->
+    <div class="home-banner-three">
+        <div class="home-slides owl-carousel owl-theme">
+            <div class="home-item item-bg">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            <div class="row align-items-center">
+                                <div class="col-lg-7">
+                                    <div class="main-banner-content">
+                                        <span>Shakuntala Foundation</span>
+                                        <h1>It is more difficult to give money away </h1>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing incididunt ut laboredolore magna aliqua elsed tempomet, consectetur adipiscing.</p>
+                                        <div class="banner-btn">
+                                            <a href="contact.html" class="default-btn">
+                                                Donate Now
+                                                <i class="flaticon-right"></i>
+                                                <span></span>
+                                            </a>
+                                            <a class="optional-btn" href="team.html">
+                                                Become Member
+                                                <i class="flaticon-right"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-5">
+                                    <div class="banner-bar">
+                                        <div class="banner-video">
+                                            <span>Watch Intro</span>
+                                            <a href="https://www.youtube.com/watch?v=uemObN8_dcw" class="video-btn popup-youtube">
+                                                <i class="bx bx-play"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="home-item item-bg2">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            <div class="row align-items-center">
+                                <div class="col-lg-7">
+                                    <div class="main-banner-content">
+                                        <span>Shakuntala Foundation</span>
+                                        <h1>It is more difficult to give money away </h1>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing incididunt ut laboredolore magna aliqua elsed tempomet, consectetur adipiscing.</p>
+                                        <div class="banner-btn">
+                                            <a href="contact.html" class="default-btn">
+                                                Donate Now
+                                                <i class="flaticon-right"></i>
+                                                <span></span>
+                                            </a>
+                                            <a class="optional-btn" href="team.html">
+                                                Become Member
+                                                <i class="flaticon-right"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-5">
+                                    <div class="banner-bar">
+                                        <div class="banner-video">
+                                            <span>Watch Intro</span>
+                                            <a href="https://www.youtube.com/watch?v=uemObN8_dcw" class="video-btn popup-youtube">
+                                                <i class="bx bx-play"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="home-item item-bg3">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            <div class="row align-items-center">
+                                <div class="col-lg-7">
+                                    <div class="main-banner-content">
+                                        <span>Shakuntala Foundation</span>
+                                        <h1>It is more difficult to give money away </h1>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing incididunt ut laboredolore magna aliqua elsed tempomet, consectetur adipiscing.</p>
+                                        <div class="banner-btn">
+                                            <a href="contact.html" class="default-btn">
+                                                Donate Now
+                                                <i class="flaticon-right"></i>
+                                                <span></span>
+                                            </a>
+                                            <a class="optional-btn" href="team.html">
+                                                Become Member
+                                                <i class="flaticon-right"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-5">
+                                    <div class="banner-bar">
+                                        <div class="banner-video">
+                                            <span>Watch Intro</span>
+                                            <a href="https://www.youtube.com/watch?v=uemObN8_dcw" class="video-btn popup-youtube">
+                                                <i class="bx bx-play"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="creative-shape">
+            <img src="assets/img/home-three/shape.png" alt="image">
+        </div>
+    </div>
+    <!-- End Home Banner Three Area -->
